7c365707cc6e84f435f50f7f431cb44939d7dc81
panglinfan <linfanpang@gmail.com>
2024-06-07 21:34:02 +0800

    feat: chat

--- src/main/java/com/riffhub/controller/MessageController.java
+++ src/main/java/com/riffhub/controller/MessageController.java
@@ -1,5 +1,6 @@
 package com.riffhub.controller;
 
+import com.riffhub.pojo.Conversation;
 import com.riffhub.pojo.Message;
 import com.riffhub.pojo.Result;
 import com.riffhub.pojo.User;
@@ -12,6 +13,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +37,19 @@ public class MessageController {
     @GetMapping("/get")
     public Result<List<Message>> getMessage(Integer user1Id, Integer user2Id) {
         return Result.success(messageService.getMessage(user1Id, user2Id));
+    }
+
+    @PostMapping("/add/conversation")
+    public Result addConversation(Integer userId, HttpServletRequest request) {
+        Map<String, Object> userInfo = JwtUtil.getLoginUserInfo(request);
+
+        Conversation conversation = conversationService.findByUser1IdAndUser2Id(userId, (Integer) userInfo.get("id"));
+        if(conversation == null) {
+            Conversation con = new Conversation();
+            con.setUser1Id(userId);
+            con.setUser2Id((Integer) userInfo.get("id"));
+            conversationService.addConversation(con);
+        }
+        return Result.success();
     }
 }
